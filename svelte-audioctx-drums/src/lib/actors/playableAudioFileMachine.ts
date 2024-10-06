869fe14bffe53a6a87bc4673b38c92f379c2dadb
Tobi Obeck <13554426+TobiObeck@users.noreply.github.com>
2024-10-06 18:58:02 +0200

    kinda working to download as stream

--- svelte-audioctx-drums/src/lib/actors/playableAudioFileMachine.ts
+++ svelte-audioctx-drums/src/lib/actors/playableAudioFileMachine.ts
@@ -1,9 +1,10 @@
-import { setup, assign, fromPromise, sendParent } from 'xstate';
+import { setup, assign, fromPromise, sendParent, fromCallback, type AnyEventObject } from 'xstate';
 
 export const playableAudioFileMachine = setup({
 	types: {
 		context: {} as {
 			audioFilePath: string;
+			progress: number;
 			file: ArrayBuffer | null;
 			error: any | null;
 		},
@@ -13,19 +14,63 @@ export const playableAudioFileMachine = setup({
 			| { type: 'pause' }
 			| { type: 'stop' }
 			| { type: 'loadSoundFile'; audioFilePath: string }
+			| { type: 'DOWNLOAD_COMPLETE'; result: any }
+			| { type: 'PROGRESS' }
 	},
 	actors: {
 		fetchAudio: fromPromise(async ({ input }: { input: { audioFilePath: string } }) => {
 			console.log('fetch afPath', input.audioFilePath);
 			const response = await fetch(input.audioFilePath);
 			return response.arrayBuffer();
-		})
+		}),
+		fetchAudioInChunks: fromCallback(
+			({
+				input,
+				sendBack
+			}: {
+				input: { audioFilePath: string };
+				sendBack: (event: AnyEventObject) => void;
+			}) => {
+				fetch(input.audioFilePath).then((response) => {
+					if (!response.body) return;
+					let receivedLength = 0;
+					const contentLength = Number(response.headers.get('Content-Length'));
+					const reader = response.body.getReader();
+
+					reader
+						.read()
+						// @ts-ignore
+						.then(function processResult(result) {
+							if (result.done) {
+								sendBack({ type: 'DOWNLOAD_COMPLETE', result: result });
+								return;
+							}
+							receivedLength += result.value.length;
+							const progress = (receivedLength / contentLength) * 100;
+							sendBack({ type: 'PROGRESS', progress });
+
+							// const temp = reader.read().then(processResult);
+							// return temp;
+							return reader.read().then(processResult);
+						})
+						.catch((error) => {
+							sendBack({ type: 'ERROR', error });
+						});
+				});
+
+				// console.log('fetch in chunks afPath', input?.audioFilePath);
+				// const response = await fetch(input?.audioFilePath);
+
+				// return response.arrayBuffer();
+			}
+		)
 	}
 }).createMachine({
 	id: 'appMachine',
 	initial: 'initial',
 	context: {
 		audioFilePath: '',
+		progress: 0,
 		file: null,
 		error: null
 	},
@@ -48,21 +93,40 @@ export const playableAudioFileMachine = setup({
 		loading: {
 			invoke: {
 				id: 'invokeFile',
-				src: 'fetchAudio',
-				input: ({ context: { audioFilePath } }) => ({ audioFilePath }),
-				onDone: {
-					target: 'ready',
+				src: 'fetchAudioInChunks',
+				input: ({ context: { audioFilePath } }) => ({ audioFilePath })
+
+				// src: 'fetchAudio',
+				// input: ({ context: { audioFilePath } }) => ({ audioFilePath }),
+				// onDone: {
+				// 	target: 'ready',
+				// 	actions: [
+				// 		({ event }) => {
+				// 			console.log('onDone!', event);
+				// 		},
+				// 		assign({ file: ({ event }) => event.output }),
+				// 		sendParent(({ self }) => ({ type: 'finishedLoading', actorRef: self.system }))
+				// 	]
+				// },
+				// onError: {
+				// 	target: 'failure',
+				// 	actions: assign({ error: ({ event }) => event.error })
+				// }
+			},
+			on: {
+				DOWNLOAD_COMPLETE: {
 					actions: [
 						({ event }) => {
-							console.log('onDone!', event);
-						},
-						assign({ file: ({ event }) => event.output }),
-						sendParent(({ self }) => ({ type: 'finishedLoading', actorRef: self.system }))
+							console.log('donwload complete', event);
+						}
 					]
 				},
-				onError: {
-					target: 'failure',
-					actions: assign({ error: ({ event }) => event.error })
+				PROGRESS: {
+					actions: [
+						({ event }) => {
+							console.log('progress', event);
+						}
+					]
 				}
 			}
 		},
